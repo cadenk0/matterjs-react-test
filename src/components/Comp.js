fd7b30ce4e9f635ae1521b866bf9c4450aeb8b89
Caden Khuu <117611429+cadenk0@users.noreply.github.com>
2024-03-09 22:19:41 -0500

    Add files via upload

---
+++ src/components/Comp.js
@@ -0,0 +1,58 @@
+import { useEffect, useRef } from "react";
+import Matter, { Bodies, Engine, Render, World } from "matter-js";
+
+import "./Comp.css";
+
+const Comp = ({ props }) => {
+  const scene = useRef();
+  const engine = useRef(Engine.create());
+
+  useEffect(() => {
+    const cw = document.body.clientWidth;
+    const ch = document.body.clientHeight;
+    const total = 10;
+
+    const render = Render.create({
+      element: scene.current,
+      engine: engine.current,
+      options: {
+        width: cw,
+        height: ch,
+        wireframes: false,
+        background: "transparent",
+      },
+    });
+
+    World.add(engine.current.world, [
+      Bodies.rectangle(cw / 2, -10, cw, 20, { isStatic: true }),
+      Bodies.rectangle(-10, ch / 2, 20, ch, { isStatic: true }),
+      Bodies.rectangle(cw / 2, ch + 10, cw, 20, { isStatic: true }),
+      Bodies.rectangle(cw + 10, ch / 2, 20, ch, { isStatic: true }),
+    ]);
+
+      World.add(engine.current.world, [
+        Bodies.circle(400, 40, 50, { 
+          restitution: 1.3,
+          friction: 1,
+
+      }),
+      ]);
+
+    Matter.Runner.run(engine.current);
+    Render.run(render);
+
+    return () => {
+      Render.stop(render);
+      World.clear(engine.current.world);
+      Engine.clear(engine.current);
+      render.canvas.remove();
+      render.canvas = null;
+      render.context = null;
+      render.textures = {};
+    };
+  }, []);
+
+  return <div ref={scene} className="component-main" />;
+};
+
+export default Comp;
